f9faf4881c5ce724317e6154885d3c0e77591e0d
Mad <github@typ03.de>
2019-12-03 23:52:11 +0100

    enable logging

--- Source/SustainableEvasion/SustainableEvasion.cs
+++ Source/SustainableEvasion/SustainableEvasion.cs
@@ -15,7 +15,7 @@ namespace SustainableEvasion
         internal static string ModDirectory;
 
         // BEN: Debug (0: nothing, 1: errors, 2:all)
-        internal static int DebugLevel = 1;
+        internal static int DebugLevel = 2;
 
         public static void Init(string directory, string settingsJSON) {
             var harmony = HarmonyInstance.Create("de.ben.SustainableEvasion");
